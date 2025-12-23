c2c33964afba784386be696cb3702a5610a41471
NAHDI51 <hossainnahdi8051@gmail.com>
2025-12-24 01:46:33 +0600

    Ported GC defs and functions from paint.c

---
+++ src/GC.c
@@ -0,0 +1,71 @@
+#include <X11/X.h>
+#include <err.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/XKBlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xft/Xft.h>
+
+#include "def.h"
+#include "GC.h"
+#include "window.h"
+#include "color.h"
+
+static GC createGraphicContent(
+    unsigned int lineWidth, 
+    Window win,
+    unsigned long fgColor, 
+    unsigned long bgColor
+) {
+    // So far, win doesn't do anything
+    XGCValues xgcValues = {
+
+        // Drawing attributes
+        .line_style = LINE_STYLE,
+        .line_width = LINE_WIDTH,
+        .cap_style  = CAP_STYLE,
+        .join_style = JOIN_STYLE,
+        .fill_style = FILL_STYLE,
+
+        // Colors
+        .foreground = fgColor,        
+        .background = bgColor
+    };           
+    unsigned long long 
+    valueMask = GCForeground | 
+                GCBackground | 
+                GCFillStyle | 
+                GCLineStyle  | 
+                GCLineWidth  | 
+                GCCapStyle   | 
+                GCJoinStyle;
+                
+    GC graphicContent = XCreateGC(disp, win, valueMask, &xgcValues);
+    return graphicContent;
+}
+
+GC takeGraphicContentInput(Window win) {
+    unsigned int lineWidth;
+    printf("Enter the width of the line: ");
+    scanf("%u", &lineWidth);
+
+    // Default color: Red foreground, Blue background
+    GC graphicContent = createGraphicContent(lineWidth, win, 
+                        colorResources[COLOR_RED]->pixel, 
+                        colorResources[COLOR_BLUE]->pixel);
+
+    printf("Graphic Content created with line width: %u\n", lineWidth);
+
+    return graphicContent;
+}
+
+/*
+    @brief: Creates a default graphic content
+    with predefined attributes.
+*/
+GC openDefaultGraphicContent(Window win) {
+    return createGraphicContent(LINE_WIDTH, win, 
+                    colorResources[COLOR_RED]->pixel,   
+                    colorResources[COLOR_BLUE]->pixel 
+    );
+}
---
+++ src/GC.h
@@ -0,0 +1,40 @@
+#ifndef __GC_H
+#define __GC_H 1    
+
+#include <X11/X.h>
+#include <err.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/XKBlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xft/Xft.h>
+
+#include "def.h"
+
+typedef enum {
+    LINE_STYLE = LineSolid,
+    LINE_WIDTH = 5,
+    CAP_STYLE  = CapRound,
+    JOIN_STYLE = JoinMiter,
+    FILL_STYLE = FillSolid,
+} defaultGCProperties;
+
+static GC createGraphicContent(
+    unsigned int lineWidth, 
+    Window win,
+    unsigned long fgColor, 
+    unsigned long bgColor
+);
+
+/*
+    @brief Takes input of lineWidth from the user
+*/
+GC takeGraphicContentInput(Window win);
+
+/*
+    @brief: Creates a default graphic content
+    with predefined attributes.
+*/
+GC openDefaultGraphicContent(Window win);
+
+#endif /* __GC_H */
