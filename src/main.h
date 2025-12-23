158bde80d56e1246f5acf8228438e940ba12d399
NAHDI51 <hossainnahdi8051@gmail.com>
2025-12-24 00:47:38 +0600

    shifted global variables and inclusions

---
+++ src/main.h
@@ -0,0 +1,14 @@
+/*
+    * def.h
+    *
+    * Created on: 2011-5-19
+    * 
+    * def.h contains the essential definitions and macros needed 
+    * across the files of the project, bundled together. It contains
+    * color definitions, window creation functions, and necessary structures
+    * for the paint application.
+*/
+
+#ifndef __DEF_H
+#define __DEF_H
+#endif
